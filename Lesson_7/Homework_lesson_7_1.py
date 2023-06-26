909d3ba6947a183878ccfc9262ca24730416332b
nastenkailina <nastia.ilina21@gmail.com>
2023-06-26 08:38:05 +0200

    commiting first files

--- Lesson_7/Homework_lesson_7_1.py
+++ Lesson_7/Homework_lesson_7_1.py
@@ -1,8 +1,8 @@
 import math  # Подключили модуль math для использования функций.
-radian_input = float(input('Сколько радиан перевести в градусы?\n>'))
 def convert_radians_to_degrees(radian_input):   # Создаем функцию, вводим параметры
     return round(radian_input * (180 / math.pi), 5)
     # Возвращаем значение функции, округляя до 5 цифр, после запятой.
+radian_input = float(input('Сколько радиан перевести в градусы?\n>'))
 result = convert_radians_to_degrees(radian_input)
 print(result)   # Выводим результат.
 
